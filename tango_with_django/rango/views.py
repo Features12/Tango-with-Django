7750b564a636d38ecff193e51e42da29c710910d
Inna_Features <features.inna@mail.ru>
2017-09-23 18:09:04 +0300

    add views for index page

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Category
 
 def index(request):
-    context_dict = {'boldmessage': "Hello Inna"}
-    return render(request, 'index.html', context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories' : category_list}
+    return render(request, 'rango/index.html', context_dict)
