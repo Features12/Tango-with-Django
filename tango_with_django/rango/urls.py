a671bf44a7c6833532cc23651397f5ece28e9509
Inna_Features <features.inna@mail.ru>
2017-10-05 13:54:29 +0300

    include new url - django-register-redux

--- tango_with_django/rango/urls.py
+++ tango_with_django/rango/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from rango import views
 from django.conf import settings
 
+
 app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
@@ -9,10 +10,10 @@ urlpatterns = [
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name = 'show_category'),
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', views.user_login, name='login'),
+    # url(r'^register/$', views.register, name='register'),
+    # url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/', views.restricted, name='restricted'),
-    url(r'^logout/$', views.user_logout, name='logout'),
+    # url(r'^logout/$', views.user_logout, name='logout'),
 ]
 
 #
